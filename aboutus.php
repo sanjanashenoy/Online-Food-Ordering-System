081fb85ec985c9c371a89254316d3e7596d36f29
Sanjana Shenoy <108812611+sanjanashenoy@users.noreply.github.com>
2023-03-12 15:21:01 +0530

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,173 @@
+<?php
+session_start();
+?>
+
+<html>
+
+  <head>
+    <title> About | GoFood </title>
+  </head>
+
+  <link rel="stylesheet" type = "text/css" href ="css/aboutus.css">
+  <link rel="stylesheet" type = "text/css" href ="css/bootstrap.min.css">
+  <script type="text/javascript" src="js/jquery.min.js"></script>
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.1/js/bootstrap.min.js" integrity="sha384-XEerZL0cuoUbHE4nZReLT7nx9gQrQreJekYhJD9WNWhH8nEW+0c5qq7aIo2Wl30J" crossorigin="anonymous"></script>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Libre+Franklin:wght@100&display=swap" rel="stylesheet">
+
+  <body>
+
+  
+    <button onclick="topFunction()" id="myBtn" title="Go to top">
+      <span class="glyphicon glyphicon-chevron-up"></span>
+    </button>
+  
+    <script type="text/javascript">
+      window.onscroll = function()
+      {
+        scrollFunction()
+      };
+
+      function scrollFunction(){
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+          document.getElementById("myBtn").style.display = "block";
+        } else {
+          document.getElementById("myBtn").style.display = "none";
+        }
+      }
+
+      function topFunction() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+      }
+    </script>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top navigation-clean-search" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#myNavbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.php">GoFood</a>
+        </div>
+
+        <div class="collapse navbar-collapse " id="myNavbar">
+          <ul class="nav navbar-nav">
+            <li><a href="index.php">Home</a></li>
+            <li class="active"><a href="aboutus.php">About</a></li>
+            <li><a href="contactus.php">Contact Us</a></li>
+          </ul>
+
+<?php
+if(isset($_SESSION['login_user1'])){
+
+?>
+
+
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#"><span class="glyphicon glyphicon-user"></span> Welcome <?php echo $_SESSION['login_user1']; ?> </a></li>
+            <li><a href="myrestaurant.php">MANAGER CONTROL PANEL</a></li>
+            <li><a href="logout_m.php"><span class="glyphicon glyphicon-log-out"></span> Log Out </a></li>
+          </ul>
+<?php
+}
+else if (isset($_SESSION['login_user2'])) {
+  ?>
+           <ul class="nav navbar-nav navbar-right">
+            <li><a href="#"><span class="glyphicon glyphicon-user"></span> Welcome <?php echo $_SESSION['login_user2']; ?> </a></li>
+            <li><a href="foodlist.php"><span class="glyphicon glyphicon-cutlery"></span> Food Zone </a></li>
+            <li><a href="cart.php"><span class="glyphicon glyphicon-shopping-cart"></span> Cart 
+            (<?php
+              if(isset($_SESSION["cart"])){
+              $count = count($_SESSION["cart"]); 
+              echo "$count"; 
+            }
+              else
+                echo "0";
+              ?>)
+            </a></li>
+            <li><a href="logout_u.php"><span class="glyphicon glyphicon-log-out"></span> Log Out </a></li>
+          </ul>
+  <?php        
+}
+else {
+
+  ?>
+
+<ul class="nav navbar-nav navbar-right">
+            <li><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-user"></span> Sign Up <span class="caret"></span> </a>
+                <ul class="dropdown-menu">
+              <li> <a href="customersignup.php"> User Sign-up</a></li>
+              <li> <a href="managersignup.php"> Manager Sign-up</a></li>
+          
+            </ul>
+            </li>
+
+            <li><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-log-in"></span> Login <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+              <li> <a href="customerlogin.php"> User Login</a></li>
+              <li> <a href="managerlogin.php"> Manager Login</a></li>
+
+            </ul>
+            </li>
+          </ul>
+
+<?php
+}
+?>
+        </div>
+
+      </div>
+    </nav>
+
+    <div>
+        
+        <div class="tagline"id="heading">Excellent taste in every bite!!</div>
+        <div id="heading"><h2>We are on a mission to make people’s lives easier and mealtime tastier.
+We take a fresh approach to the food industry by delivering hygienic, delicious, better-for-you, homely meals, right to your door. We are here so that you can finally ditch all the grocery shopping, cutting, cooking and cleaning and take mealtime stress off your plate. GoFood enables you to have your daily meals without skimping on variety or great taste. All of it, brought to you on a platform that is wondrously easy to use and at no regret prices.</h2>
+        <img src="images/FoodrLogo.jpeg">
+        </div>
+      
+    </div>
+    <!-- <section id="testimonials">
+    <div id="testimonial-carousel" class="carousel slide" data-ride="false">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+         <img class="testimonial-image" src="images/pizza_pie.jpeg" alt="pizza">
+          
+
+
+        </div>
+        <div class="carousel-item">
+          
+          <img class="testimonial-image" src="images/pancake_pie.jpeg" alt="lady-profile">
+        
+
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#testimonial-carousel" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon"></span>
+
+      </a>
+      <a class="carousel-control-next" href="#testimonial-carsousel" role="button" data-slide="next">
+        <span class="carousel-control-next-icon"></span>
+
+      </a>
+    </div>
+
+
+
+
+
+  </section> -->
+
+      <!-- <iframe src="https://www.google.com/maps/place/Le+Cafe/@11.9316155,79.8335933,17z/data=!3m1!4b1!4m5!3m4!1s0x3a53636a24873e45:0x7f1e7ad6a30982dd!8m2!3d11.9316103!4d79.835782" style="width: 250px"></iframe> -->
+         </body>
+</html>
---
+++ foodorder_trig_procedures.sql
@@ -0,0 +1,335 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 12, 2022 at 06:13 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `foodorder`
+--
+
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `getOrders` (IN `RID` INT)  SELECT order_ID,F_ID,foodname,price,quantity,order_date,username, R_ID FROM orders WHERE R_ID = RID$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact`
+--
+
+CREATE TABLE `contact` (
+  `Name` varchar(250) NOT NULL,
+  `Email` varchar(250) NOT NULL,
+  `Mobile` varchar(250) NOT NULL,
+  `Subject` varchar(250) NOT NULL,
+  `Message` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `contact`
+--
+
+INSERT INTO `contact` (`Name`, `Email`, `Mobile`, `Subject`, `Message`) VALUES
+('CHANDAN KUMAR', 'ckj40856@gmail.com', '9487810674', 'sa', ''),
+('CHANDAN KUMAR', 'ckj40856@gmail.com', '9487810674', 'sa', ''),
+('BIRJU KUMAR', 'ckj40856@gmail.com', '8903079750', 'asd', 'asdasdasd'),
+('CHANDAN KUMAR', 'ckj40856@gmail.com', '9487810674', 'asd', 'hfgdsfsx');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `username` varchar(30) NOT NULL,
+  `fullname` varchar(30) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `contact` varchar(30) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `password` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `customer`
+--
+
+INSERT INTO `customer` (`username`, `fullname`, `email`, `contact`, `address`, `password`) VALUES
+('joshita', 'Joshita', 'josh@gmail.com', '9876549876', 'Bangalore', '123'),
+('Raksh', 'Rakshita', 'raksh@gmail.com', '7896547890', 'RR Nagar,Bangalore', '12345');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `food`
+--
+
+CREATE TABLE `food` (
+  `F_ID` int(30) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `price` int(30) NOT NULL,
+  `description` varchar(200) NOT NULL,
+  `R_ID` int(30) NOT NULL,
+  `images_path` varchar(200) NOT NULL,
+  `options` varchar(10) NOT NULL DEFAULT 'ENABLE'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `food`
+--
+
+INSERT INTO `food` (`F_ID`, `name`, `price`, `description`, `R_ID`, `images_path`, `options`) VALUES
+(80, 'Open Butter Masala Dosa', 100, 'Potato masala smeared generously all over the dosa and topped with butter ', 7, 'images/dosa_kai.jpeg', 'ENABLE'),
+(82, 'Chole Bhature', 120, 'A combination of chana masala (spicy white chickpeas) and bhatura/puri, a fried bread made from maida', 7, 'images/chole_kai.jpeg', 'ENABLE'),
+(83, 'Idly', 60, 'Idli is a soft, pillowy steamed savory cake made from rice and lentil batter. ', 7, 'images/idly_kai.jpeg', 'ENABLE'),
+(84, 'Coffee', 30, 'Beverage brewed from the roasted and ground seeds of the tropical evergreen coffee plants of African origin.', 7, 'images/coffee_kai.jpeg', 'ENABLE'),
+(85, 'Tea', 30, 'Tea is an aromatic beverage prepared by pouring hot or boiling water over cured or fresh leaves of Camellia sinensis', 7, 'images/tea_kai.jpeg', 'ENABLE'),
+(86, 'Waffle', 170, 'Waffle, crisp raised cake baked in a waffle iron', 8, 'images/waffle_pie.jpeg', 'ENABLE'),
+(87, 'Pancakes', 200, 'A pancake is a thin, flat, circular piece of cooked batter made from milk, flour, and eggs.', 8, 'images/pancake_pie.jpeg', 'ENABLE'),
+(88, 'Sizzling Brownie', 200, 'It is a chocolate brownie with a scoop of ice-cream on top served with a generous pouring of melted chocolate on the ice-cream.', 8, 'images/brownie_pie.jpeg', 'ENABLE'),
+(89, 'Pizza', 300, 'Pizza, dish of Italian origin consisting of a flattened disk of bread dough topped with some combination of olive oil, oregano, tomato, olives, mozzarella or other cheese, and many other ingredients', 8, 'images/pizza_pie.jpeg', 'ENABLE'),
+(90, 'Veg Burger', 150, 'Delicious burger with crispy and spicy paneer patty', 8, 'images/burger_pie.jpeg', 'ENABLE'),
+(92, 'Pasta', 170, 'Marinara and Alfredo Sauce mixed together with Penne', 9, 'images/pasta_ross.jpeg', 'ENABLE'),
+(93, 'Noodles', 190, 'Flavorful noodles tossed with vegetables.', 9, 'images/noodles_ross.jpeg', 'ENABLE');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `manager`
+--
+
+CREATE TABLE `manager` (
+  `username` varchar(30) NOT NULL,
+  `fullname` varchar(30) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `contact` varchar(30) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `password` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `manager`
+--
+
+INSERT INTO `manager` (`username`, `fullname`, `email`, `contact`, `address`, `password`) VALUES
+('Joshita', 'Joshita ', 'joshita.atmakuri@gmail.com', '9802927828', 'Bangalore', '123'),
+('Pai', 'Adithya Pai', 'pai@gmail.com', '8765945679', 'Jayanagar,Bangalore', '123'),
+('Ross', 'Ross', 'ross@gmail.com', '9876078548', 'Church Street', '123');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `order_ID` int(30) NOT NULL,
+  `F_ID` int(30) NOT NULL,
+  `foodname` varchar(30) NOT NULL,
+  `price` int(30) NOT NULL,
+  `quantity` int(30) NOT NULL,
+  `order_date` date NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `R_ID` int(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`order_ID`, `F_ID`, `foodname`, `price`, `quantity`, `order_date`, `username`, `R_ID`) VALUES
+(69, 80, 'Open Butter Masala Dosa', 100, 1, '2022-01-09', 'Raksh', 7),
+(70, 86, 'Waffle', 170, 1, '2022-01-09', 'Raksh', 8),
+(71, 93, 'Noodles', 190, 1, '2022-01-09', 'Raksh', 9),
+(72, 80, 'Open Butter Masala Dosa', 100, 1, '2022-01-09', 'Raksh', 7),
+(73, 86, 'Waffle', 170, 1, '2022-01-09', 'Raksh', 8),
+(74, 93, 'Noodles', 190, 2, '2022-01-09', 'Raksh', 9),
+(75, 83, 'Idly', 60, 1, '2022-01-09', 'Raksh', 7),
+(76, 83, 'Idly', 60, 1, '2022-01-10', 'Raksh', 7),
+(77, 83, 'Idly', 60, 1, '2022-01-10', 'Raksh', 7),
+(78, 82, 'Chole Bhature', 120, 1, '2022-01-10', 'Raksh', 7),
+(79, 82, 'Chole Bhature', 120, 1, '2022-01-10', 'Raksh', 7),
+(80, 88, 'Sizzling Brownie', 200, 1, '2022-01-10', 'joshita', 8),
+(81, 86, 'Waffle', 170, 1, '2022-01-10', 'joshita', 8),
+(82, 87, 'Pancakes', 200, 1, '2022-01-11', 'joshita', 8),
+(83, 84, 'Coffee', 30, 1, '2022-01-11', 'joshita', 7),
+(84, 82, 'Chole Bhature', 120, 1, '2022-01-11', 'joshita', 7),
+(85, 87, 'Pancakes', 200, 1, '2022-01-11', 'joshita', 8),
+(86, 84, 'Coffee', 30, 1, '2022-01-11', 'joshita', 7),
+(87, 82, 'Chole Bhature', 120, 1, '2022-01-11', 'joshita', 7),
+(88, 83, 'Idly', 60, 1, '2022-01-11', 'joshita', 7),
+(89, 87, 'Pancakes', 200, 1, '2022-01-11', 'joshita', 8),
+(90, 84, 'Coffee', 30, 1, '2022-01-11', 'joshita', 7),
+(91, 82, 'Chole Bhature', 120, 1, '2022-01-11', 'joshita', 7),
+(92, 83, 'Idly', 60, 1, '2022-01-11', 'joshita', 7),
+(93, 80, 'Open Butter Masala Dosa', 100, 1, '2022-01-11', 'joshita', 7),
+(94, 89, 'Pizza', 300, 1, '2022-01-11', 'joshita', 8),
+(95, 92, 'Pasta', 170, 1, '2022-01-11', 'joshita', 9),
+(98, 83, 'Idly', 60, 1, '2022-01-11', 'joshita', 7),
+(99, 93, 'Noodles', 190, 1, '2022-01-11', 'joshita', 9);
+
+--
+-- Triggers `orders`
+--
+DELIMITER $$
+CREATE TRIGGER `insertLog` AFTER INSERT ON `orders` FOR EACH ROW INSERT INTO view_orders VALUES(New.Order_id,NEW.F_id,NEW.foodname,NEW.price,NEW.quantity,now(),NEW.username,NEW.R_id)
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `restaurants`
+--
+
+CREATE TABLE `restaurants` (
+  `R_ID` int(30) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `contact` varchar(30) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `M_ID` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `restaurants`
+--
+
+INSERT INTO `restaurants` (`R_ID`, `name`, `email`, `contact`, `address`, `M_ID`) VALUES
+(7, 'Kai Ruchi', 'kairuchi@gmail.com', '9870966800', 'JP Nagar, Bangalore', 'Joshita'),
+(8, 'Pai\'s Pies', 'pies@gmail.com', '7890534789', 'Jayanagar,Bangalore', 'Pai'),
+(9, 'Ross Cafe', 'ross_cafe@gmail.com', '8769876543', 'Church Street', 'Ross');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `view_orders`
+--
+
+CREATE TABLE `view_orders` (
+  `order_ID` int(30) NOT NULL,
+  `F_ID` int(30) NOT NULL,
+  `foodname` varchar(30) NOT NULL,
+  `price` int(30) NOT NULL,
+  `quantity` int(30) NOT NULL,
+  `order_date` date NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `R_ID` int(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `view_orders`
+--
+
+INSERT INTO `view_orders` (`order_ID`, `F_ID`, `foodname`, `price`, `quantity`, `order_date`, `username`, `R_ID`) VALUES
+(98, 83, 'Idly', 60, 1, '2022-01-11', 'joshita', 7),
+(99, 93, 'Noodles', 190, 1, '2022-01-11', 'joshita', 9);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- Indexes for table `food`
+--
+ALTER TABLE `food`
+  ADD PRIMARY KEY (`F_ID`,`R_ID`),
+  ADD KEY `R_ID` (`R_ID`);
+
+--
+-- Indexes for table `manager`
+--
+ALTER TABLE `manager`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`order_ID`),
+  ADD KEY `F_ID` (`F_ID`),
+  ADD KEY `username` (`username`),
+  ADD KEY `R_ID` (`R_ID`);
+
+--
+-- Indexes for table `restaurants`
+--
+ALTER TABLE `restaurants`
+  ADD PRIMARY KEY (`R_ID`),
+  ADD UNIQUE KEY `M_ID_2` (`M_ID`),
+  ADD KEY `M_ID` (`M_ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `food`
+--
+ALTER TABLE `food`
+  MODIFY `F_ID` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=94;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `order_ID` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=100;
+
+--
+-- AUTO_INCREMENT for table `restaurants`
+--
+ALTER TABLE `restaurants`
+  MODIFY `R_ID` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `food`
+--
+ALTER TABLE `food`
+  ADD CONSTRAINT `food_ibfk_1` FOREIGN KEY (`R_ID`) REFERENCES `restaurants` (`R_ID`);
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`F_ID`) REFERENCES `food` (`F_ID`),
+  ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`username`) REFERENCES `customer` (`username`),
+  ADD CONSTRAINT `orders_ibfk_3` FOREIGN KEY (`R_ID`) REFERENCES `restaurants` (`R_ID`);
+
+--
+-- Constraints for table `restaurants`
+--
+ALTER TABLE `restaurants`
+  ADD CONSTRAINT `restaurants_ibfk_1` FOREIGN KEY (`M_ID`) REFERENCES `manager` (`username`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
